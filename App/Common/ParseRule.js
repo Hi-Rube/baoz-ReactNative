8b0903db336db544b5d276075dbed8b9507cebd5
Rube <353371737@qq.com>
2015-04-20 01:35:25 +0800

    评论内容HTML解析-1

---
+++ App/Common/ParseRule.js
@@ -0,0 +1,54 @@
+module.exports = {
+  '<h1>': {
+    fontSize: 36,
+  },
+  '<h2>': {
+    fontSize: 30,
+  },
+  '<h3>': {
+    fontSize: 24,
+  },
+  '<h4>': {
+    fontSize: 18,
+  },
+  '<h5>': {
+    fontSize: 14,
+  },
+  '<h6>': {
+    fontSize: 12,
+  },
+  '<blockquote>': {
+    borderLeftWidth: 5,
+    borderColor: '#ccc',
+    paddingLeft: 10,
+  },
+  '<ol>': {
+    marginLeft: 10,
+  },
+  '<li>': {},
+  '<strong>': {
+    fontWeigth: 'bold',
+  },
+  '<p>': {
+    fontSize: 12,
+    color: '#333',
+    padding: 0,
+    margin: 0
+  },
+  '<pre>': {
+    backgroundColor: '#efefef'
+  },
+  '<code>': {
+    backgroundColor: '#f6f6f6',
+    color: '#c7254e',
+    paddingTop: 2,
+    paddingBottom: 2,
+    paddingLeft: 4,
+    paddingRight: 4,
+    borderRadius: 3,
+  },
+  '<div>': {
+    color: '#333',
+    fontSize: 12
+  }
+};
